145175579644bee9291827227f52b47f6ba799bb
Cainã Costa <me@cfcosta.com>
2024-09-26 19:46:39 -0300

    fixup! chore: nix setup

--- flake.nix
+++ flake.nix
@@ -5,16 +5,28 @@
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
+    {
+      nixpkgs,
+      flake-utils,
+
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.default = pkgs.buildGo123Module {
+          pname = "wot-relay";
+          version = builtins.hashFile "sha256" ./go.sum;
+          src = ./.;
+          vendorHash = "sha256-XDtWQEyRomWXJ2P4xXY+s2NEx6vRFzKFG6/r5suEiSs=";
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            go
+            go_1_23
           ];
         };
       }
